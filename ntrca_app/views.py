55a878c75e397ae0324a99955ecad827c5a075fe
Rayhun <rayhunkhan27@gmail.com>
2021-10-28 22:09:54 +0600

    update view

--- ntrca_app/views.py
+++ ntrca_app/views.py
@@ -1,15 +1,9 @@
-import pandas as pd
-import shutil
 from django.shortcuts import render, redirect, HttpResponse
 from django.views import View
 from django.core.paginator import Paginator
 
-from .models import (
-    NTRCACirtificate, PostAndSubjectCode, StudentValidData, StudentWrongData, Subject,
-    TotalStudent
-)
-from .filters import CandidateFilter
-# from .pdf import render_pdf
+from .models import NTRCACirtificate
+
 
 def registration(number):
     if number <= 9:
@@ -38,30 +32,19 @@ class NtrcaInputDistrict(View):
         return render(request, template_name)
     def post(self, request):
         template_name = 'district_input.html'
+
         district = request.POST.get('district')
         roll = request.POST.get('roll')
         district_distribution = request.POST.get('district_distribution')
-        all_district = NTRCACirtificate.objects.filter(permanent_dist=district)
-        district_qs = NTRCACirtificate.objects.filter(
-            permanent_dist=district_distribution
-        )
-        try:
-            single_roll = NTRCACirtificate.objects.get(roll=roll)
-        except Exception as e:
-            print(e)
-            single_roll = None
-        if all_district.last():
-            request.session['all_district'] = [
-                district.permanent_dist for district in all_district
-            ]
+
+        if district:
+            request.session['all_district'] = district
             return redirect('ntrca_cirtificate_download')
-        elif single_roll is not None:
-            request.session['single_roll'] = single_roll.roll
+        elif roll:
+            request.session['single_roll'] = roll
             return redirect('ntrca_single_data')
-        elif district_qs.last():
-            request.session['all_data'] = [
-                district.permanent_dist for district in district_qs
-            ]
+        elif district_distribution:
+            request.session['all_data'] = district_distribution
             return redirect('ntrca_district_distribution')
         else:
             message = "Data Not Match"
@@ -71,8 +54,8 @@ class NtrcaInputDistrict(View):
 class NtrcaDistrictDistribution(View):
     def get(self, request):
         template_name = 'district_distribution.html'
-        all_data = request.session.get('all_data')[:1]
-        all_datas = NTRCACirtificate.objects.filter(permanent_dist=all_data[0])
+        all_data = request.session.get('all_data')
+        all_datas = NTRCACirtificate.objects.filter(permanent_dist=all_data)
         subject_list = []
         for data in all_datas:
             if data.subject_code not in subject_list:
@@ -81,11 +64,12 @@ class NtrcaDistrictDistribution(View):
         subject_wise_data = []
         for subject in subject_list:
             sub = NTRCACirtificate.objects.filter(
-                subject_code=subject,permanent_dist=all_data[0]
+                subject_code=subject,permanent_dist=all_data
             )
             subject_wise_data.append(sub)
         context = {
             'subject_wise_data': subject_wise_data,
+            'district': all_data,
         }
         return render(request, template_name, context)
 
@@ -108,9 +92,9 @@ class NtrcaSingleData(View):
 class NTRCACirtificateDownloadView(View):
     def get(self, request):
         template_name = 'certificate.html'
-        all_district = request.session.get('all_district')[:1]
+        all_district = request.session.get('all_district')
         if all_district:
-            qs = NTRCACirtificate.objects.filter(permanent_dist=all_district[0])
+            qs = NTRCACirtificate.objects.filter(permanent_dist=all_district)
             paginator = Paginator(qs, 100)
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
@@ -120,77 +104,6 @@ class NTRCACirtificateDownloadView(View):
         return render(request, template_name, context)
 
 
-
 class NTRCACirtificateView(View):
     def get(self, request):
-        template_name = 'certificate.html'
-        # read_exl = pd.read_csv("E:/Official/test/ntrca_app/excel/result.csv")
-        # excel_list = read_exl.values.tolist()
-        # print(excel_list)
-        total = 0
-        dup = 0
-        valid_data = 0
-
-        totaldtat = TotalStudent.objects.all()
-
-
-        for data in totaldtat:
-            try:
-                student = NTRCACirtificate.objects.get(roll=data.roll)
-                student.written_number = data.score
-                student.save()
-
-                StudentValidData.objects.create(
-                    roll=data.roll,
-                    subject_code=data.subject_code,
-                    score=data.score,
-                )
-                valid_data += 1
-            except Exception as e:
-                print(e)
-                StudentWrongData.objects.create(
-                    roll=data.roll,
-                    subject_code=data.subject_code,
-                    score=data.score,
-                )
-                dup += 1
-        #         StudentValidData.objects.get(roll=data[0])
-        #         StudentWrongData.objects.create(
-        #             roll=data[0],
-        #             subject_code=data[1],
-        #             score=data[2],
-        #         )
-        #         print(f"Duplicate Data")
-        #         dup += 1
-        #     except Exception as e:
-        #         print(e)
-        #         TotalStudent.objects.create(
-        #             roll=data[0],
-        #             subject_code=data[1],
-        #             score=data[2],
-        #         )
-        #         valid_data += 1
-
-            # user = NTRCACirtificate.objects.get(roll=data[0])
-            # print(f"Post Code{data[2]} subject code {data[3]}")
-            # post = PostAndSubjectCode.objects.get(post_code=data[2], subject_code=data[3])
-            # user.post_name = post.post_name
-            # user.post_code = post.post_code
-            # user.institute_type = post.institute_type
-            # user.save()
-
-            total += 1
-            print(f"Total Number {total} Valid Data {valid_data} Invalid Data {dup}")
         return HttpResponse("Success")
-
-
-class ImageMoveOtherFolder(View):
-    def get(self, request):
-        ntrca_certificate = NTRCACirtificate.objects.all()
-        for data in ntrca_certificate:
-            source = f"E:/Official/test/ntrca_app/static/photo/{data.invoice}.jpg"
-            dastination = f"E:/Official/test/ntrca_app/media/{data.invoice}.jpg"
-            shutil.move(source, dastination)
-            print(data.invoice)
-        return HttpResponse("Success")
-
