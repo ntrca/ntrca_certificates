9e3310fc4556eddf3deffc5525b1ad4b7009817e
Rayhan <rayhunkhan27@gmail.com>
2022-08-02 12:12:45 +0600

    update candidate models

--- candidate/models.py
+++ candidate/models.py
@@ -1,19 +1,32 @@
 from django.db import models
 
-from ntrca_app.utilities import ExamsName
+from ntrca_app.utilities import ExamsName, District, Thana, PostOffice
+
+
+GENDER = (
+    (1, 'Male'),
+    (2, 'Female'),
+    (3, 'Both'),
+)
 
 
 class Candidate(models.Model):
+    invoice = models.CharField(max_length=20, blank=True, null=True)
     exam_name = models.ForeignKey(
         ExamsName, on_delete=models.SET_NULL, null=True, blank=True,
+        related_name="candidate_exam_name"
     )
     board = models.IntegerField(blank=True, null=True)
     interview_date = models.DateField(blank=True, null=True)
     roll = models.IntegerField(blank=True, null=True)
+    reg = models.CharField(null=True, max_length=50)
     subject_code = models.IntegerField(blank=True, null=True)
+    subject_name = models.CharField(max_length=200, blank=True, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     father = models.CharField(max_length=200, blank=True, null=True)
     mother = models.CharField(max_length=200, blank=True, null=True)
+    gender = models.PositiveIntegerField(choices=GENDER, null=True)
+    image = models.ImageField(upload_to='image/', blank=True, null=True)
     s_exam = models.CharField(max_length=200, blank=True, null=True)
     s_result = models.CharField(max_length=200, blank=True, null=True)
     s_result_text = models.CharField(max_length=200, blank=True, null=True)
@@ -21,7 +34,19 @@ class Candidate(models.Model):
     h_result = models.CharField(max_length=200, blank=True, null=True)
     h_result_text = models.CharField(max_length=200, blank=True, null=True)
     dob = models.DateField(blank=True, null=True)
-    invoice = models.CharField(max_length=20, blank=True, null=True)
+    written_number = models.CharField(max_length=200, null=True)
+    post_code = models.IntegerField(null=True, blank=True)
+    post_name = models.CharField(max_length=250, null=True)
+    institute_type = models.CharField(max_length=255, null=True)
+    nid = models.CharField(max_length=255, null=True, blank=True)
+    religion = models.CharField(max_length=50, null=True)
+    permanent_vill = models.TextField(max_length=255, null=True)
+    permanent_district = models.ForeignKey(District, on_delete=models.CASCADE,
+        null=True, related_name='district')
+    permanent_police_station = models.ForeignKey(Thana, on_delete=models.CASCADE,
+        null=True, related_name='thana')
+    permanent_post_office = models.ForeignKey(PostOffice, on_delete=models.CASCADE,
+        null=True, related_name='post')
 
 
     class Meta:
